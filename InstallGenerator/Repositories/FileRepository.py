1e75a86e0d30a7c7f7179c73e1a59b741fe3f0c8
FryMondo <ptachyksergei@gmail.com>
2024-12-10 02:21:13 +0200

    updated file saving; added iterator creation

--- InstallGenerator/Repositories/FileRepository.py
+++ InstallGenerator/Repositories/FileRepository.py
@@ -1,11 +1,27 @@
+from InstallGenerator.Iterator.Aggregate import Aggregate
+
 class FileRepository:
-    def __init__(self, directory_repository):
-        self.id = None
+    def __init__(self):
+        self.id = 0
         self.file_name = None
-        self.directory_repository = directory_repository
+        self.directory_repository = None
+        self.file_type = None
+        self.files = []
 
-    def save(self):
-        pass
+    def add_file(self, file_name, path, file_type):
+        self.id += 1
+        self.file_name = file_name
+        self.directory_repository = path
+        self.file_type = file_type
+        file = {
+            "id": self.id,
+            "file_name": file_name,
+            "file_path": path,
+            "file_type": file_type
+        }
+        self.files.append(file)
 
-    def get(self, file_id):
-        pass
+    # Використання Aggregate для створення ітератора
+    def create_iterator(self):
+        aggregate = Aggregate(self)
+        return aggregate.create_iterator()
