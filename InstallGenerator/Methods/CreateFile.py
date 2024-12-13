34996618c7df03cdd4b5378df447aca8fb876ee1
FryMondo <ptachyksergei@gmail.com>
2024-12-13 23:58:14 +0200

    saving file using FileBuilder() FileDirector()

--- InstallGenerator/Methods/CreateFile.py
+++ InstallGenerator/Methods/CreateFile.py
@@ -1,3 +1,10 @@
-def createFile():
-    # Метод для створення файлу та деінсталятора
-    pass
+from InstallGenerator.Builder.FileBuilder import FileBuilder
+from InstallGenerator.Builder.FileDirector import FileDirector
+
+def createFile(file_repository, file_name, path, file_type):
+    builder = FileBuilder()
+    director = FileDirector(builder)
+
+    file = director.construct_file(file_name, path, file_type)
+
+    file_repository.add_file(file)
