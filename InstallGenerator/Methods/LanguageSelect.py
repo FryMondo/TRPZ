e417133cde2be18b7622041925e247ffd4c293e4
FryMondo <ptachyksergei@gmail.com>
2024-12-24 00:37:20 +0200

    updated method

--- InstallGenerator/Methods/LanguageSelect.py
+++ InstallGenerator/Methods/LanguageSelect.py
@@ -1,3 +1,20 @@
-def languageSelect():
-    # Метод для вибору мови інсталятора
-    pass
+from InstallGenerator.Bridge.EnglishLanguage import EnglishLanguage
+from InstallGenerator.Bridge.InstallerLanguage import InstallerLanguage
+from InstallGenerator.Bridge.UkrainianLanguage import UkrainianLanguage
+
+# Метод для вибору мови інсталятора
+def languageSelect(language):
+    implementor = EnglishLanguage()
+
+    if language == "Українська":
+        implementor = UkrainianLanguage()
+
+    return InstallerLanguage(implementor)
+
+def set_language(language):
+    global selected_language
+    selected_language = language
+
+def get_language():
+    global selected_language
+    return selected_language
