9749c97d757ad07e1f8dc421750d5582c9b1f797
FryMondo <ptachyksergei@gmail.com>
2024-12-24 19:12:28 +0200

    added new translation

--- InstallGenerator/Bridge/EnglishLanguage.py
+++ InstallGenerator/Bridge/EnglishLanguage.py
@@ -6,7 +6,10 @@ class EnglishLanguage(LanguageImplementor):
         "checking_files": "Checking installed files...",
         "files_checked": "All files checked.",
         "creating_shortcut": "Creating shortcut...",
-        "shortcut_created": "Shortcut '{name}' created in '{path}'"
+        "shortcut_created": "Shortcut '{name}' created in '{path}'",
+        "enter_license_key": "Enter the licence key: ",
+        "license_key_valid": "Licence key is valid. Installation continues...",
+        "license_key_invalid": "Invalid licence key. Please try again."
     }
 
     def get_message(self, key):
--- InstallGenerator/Bridge/UkrainianLanguage.py
+++ InstallGenerator/Bridge/UkrainianLanguage.py
@@ -6,7 +6,10 @@ class UkrainianLanguage(LanguageImplementor):
         "checking_files": "Перевірка встановлених файлів...",
         "files_checked": "Всі файли перевірено",
         "creating_shortcut": "Створення ярлика...",
-        "shortcut_created": "Ярлик '{name}' створено в '{path}'"
+        "shortcut_created": "Ярлик '{name}' створено в '{path}'",
+        "enter_license_key": "Введіть ліцензійний ключ: ",
+        "license_key_valid": "Ліцензійний ключ вірний. Продовження інсталяції...",
+        "license_key_invalid": "Невірний ліцензійний ключ. Спробуйте ще раз."
     }
 
     def get_message(self, key):
