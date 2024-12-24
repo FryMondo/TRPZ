9f340c1bf1b81b7aed80a774f839c2a01380b173
FryMondo <ptachyksergei@gmail.com>
2024-12-24 19:14:12 +0200

    renamed license_key_repo

--- InstallGenerator/Repositories/InstallerRepository.py
+++ InstallGenerator/Repositories/InstallerRepository.py
@@ -1,11 +1,11 @@
 class InstallerRepository:
-    def __init__(self, file_repo, directory_repo, licence_key_repo, language_repo, uninstaller_repo):
+    def __init__(self, file_repo, directory_repo, license_key_repo, language_repo, uninstaller_repo):
         self.id = None
         self.isShortcut = False
         self._directory_repo = directory_repo
         self._uninstaller_repo = uninstaller_repo
         self._file_repo = file_repo
-        self._licence_key_repo = licence_key_repo
+        self._license_key_repo = license_key_repo
         self._language_repo = language_repo
 
     def get_directory_repo(self):
@@ -17,8 +17,8 @@ class InstallerRepository:
     def get_file_repo(self):
         return self._file_repo
 
-    def get_licence_key_repo(self):
-        return self._licence_key_repo
+    def get_license_key_repo(self):
+        return self._license_key_repo
 
     def get_language_repo(self):
         return self._language_repo
